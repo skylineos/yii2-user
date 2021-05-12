cb4681bbee223c82564a03d9aaf4931284643e4e
Damien Kemens <dkemens@skylinenet.net>
2021-05-12 12:04:27 -0400

    Removed testing artifcats

--- tests/functional/loginPageCest.php
+++ tests/functional/loginPageCest.php
@@ -1,7 +1,5 @@
 <?php
 
-use skyline\yii\user\tests\fixtures\UserFixture;
-
 class loginPageCest
 {
     public function _before(FunctionalTester $I)
@@ -18,5 +16,17 @@ class loginPageCest
         $I->fillField('#loginform-username', 'asdf');
         $I->click('#kt_login_signin_submit');
         $I->see('Username is not a valid email address.');
+    }
+
+    public function grid(FunctionalTester $I)
+    {
+        $I->haveFixtures([
+            'twentyOneUsers' => [
+                'class' => UserFixture::classname(),
+                'dataFile' => codecept_data_dir() . 'user-21rows.php',
+            ],
+        ]);
+        $I->amOnPage('/cms/user/index');
+        $I->see('cat');
     }
 }
