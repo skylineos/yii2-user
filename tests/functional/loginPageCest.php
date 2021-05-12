7e8cc9cdda37c0782322d964f2bbca3c2e749858
Damien Kemens <dkemens@skylinenet.net>
2021-05-12 12:04:48 -0400

    Removed debug/testing artifacts

--- tests/functional/loginPageCest.php
+++ tests/functional/loginPageCest.php
@@ -17,16 +17,4 @@ class loginPageCest
         $I->click('#kt_login_signin_submit');
         $I->see('Username is not a valid email address.');
     }
-
-    public function grid(FunctionalTester $I)
-    {
-        $I->haveFixtures([
-            'twentyOneUsers' => [
-                'class' => UserFixture::classname(),
-                'dataFile' => codecept_data_dir() . 'user-21rows.php',
-            ],
-        ]);
-        $I->amOnPage('/cms/user/index');
-        $I->see('cat');
-    }
 }
