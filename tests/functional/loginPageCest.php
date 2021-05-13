6fc18b2db1db147a1139c555da9850d93b94905c
Damien Kemens <dkemens@skylinenet.net>
2021-05-13 16:54:58 -0400

    merging

--- tests/functional/loginPageCest.php
+++ tests/functional/loginPageCest.php
@@ -1,5 +1,7 @@
 <?php
 
+use skyline\yii\user\tests\fixtures\UserFixture;
+
 class loginPageCest
 {
     public function _before(FunctionalTester $I)
@@ -16,5 +18,17 @@ class loginPageCest
         $I->fillField('#loginform-username', 'asdf');
         $I->click('#kt_login_signin_submit');
         $I->see('Username is not a valid email address.');
+    }
+
+    public function grid(FunctionalTester $I)
+    {
+        $I->haveFixtures([
+            'twentyOneUsers' => [
+                'class' => UserFixture::classname(),
+                'dataFile' => codecept_data_dir() . 'user-21rows.php',
+            ],
+        ]);
+        $I->amOnPage('/cms/user/index');
+        $I->see('cat');
     }
 }
